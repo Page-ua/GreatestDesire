50a9245e4cf870fd67b84aa7c998e3251d45db9a
dimafreund <dimafreund@ukr.net>
2017-12-07 18:43:59 +0200

    first commit

---
+++ protected/config/dynamic.php
@@ -0,0 +1,63 @@
+<?php return array (
+  'components' => 
+  array (
+    'db' => 
+    array (
+      'class' => 'yii\\db\\Connection',
+      'dsn' => 'mysql:host=localhost;dbname=humhub',
+      'username' => 'root',
+      'password' => 'LongPass',
+      'charset' => 'utf8',
+    ),
+    'user' => 
+    array (
+    ),
+    'mailer' => 
+    array (
+      'transport' => 
+      array (
+        'class' => 'Swift_MailTransport',
+      ),
+      'view' => 
+      array (
+        'theme' => 
+        array (
+          'name' => 'HumHub',
+          'basePath' => '/var/www/html/themes/HumHub',
+          'publishResources' => false,
+        ),
+      ),
+    ),
+    'cache' => 
+    array (
+      'class' => 'yii\\caching\\FileCache',
+      'keyPrefix' => 'humhub',
+    ),
+    'view' => 
+    array (
+      'theme' => 
+      array (
+        'name' => 'HumHub',
+        'basePath' => '/var/www/html/themes/HumHub',
+        'publishResources' => false,
+      ),
+    ),
+  ),
+  'params' => 
+  array (
+    'installer' => 
+    array (
+      'db' => 
+      array (
+        'installer_hostname' => 'localhost',
+        'installer_database' => 'humhub',
+      ),
+    ),
+    'config_created_at' => 1512664305,
+    'horImageScrollOnMobile' => '1',
+    'databaseInstalled' => true,
+    'installed' => true,
+  ),
+  'name' => 'Greatest Desire',
+  'language' => 'ru',
+); ?>
